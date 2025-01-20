b67f4487bf869a32513f4663023627a85acef238
Yohannes-J <yohannesjohn126@gmail.com>
2025-01-20 17:11:01 +0300

    first commit

---
+++ inputCode.swift
@@ -0,0 +1,10 @@
+var sum = 0
+var number = 1
+
+// While loop to add numbers from 1 to 100
+while number <= 100 {
+    sum += number
+    number += 1
+}
+
+print("The sum of numbers from 1 to 100 is: \(sum)")
